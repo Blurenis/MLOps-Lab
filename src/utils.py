54a0692d7bcead18ed8dceb87260ce1055f085e7
Blurenis <jeremieondzaghe@gmail.com>
2025-10-19 09:53:12 +0200

    src file with add function

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+
+
+
+def add(*input):
+    
+    sum = 0
+    for element in input:
+        sum += element
+    
+    return sum
+
+def subtract(*input):
+    
+    sum = 0
+    for element in input:
+        sum += element
+    
+    return sum
+
+
+print(add(1, 2, 3))
