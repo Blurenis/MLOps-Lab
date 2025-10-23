02a9925e67b657550e84b0308f427e592569e562
justasecond241@gmail.com <justasecond241@gmail.com>
2025-10-23 11:13:46 +0200

    Feature add and multiply

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+def multiply(*input):
+    product = 1
+    for element in input:
+        product *= element
+
+    return product
+
+
+def divide(*input):
+    if not input:
+        return None
+
+    quotient = input[0]
+    for element in input[1:]:
+        quotient /= element
+
+    return quotient
