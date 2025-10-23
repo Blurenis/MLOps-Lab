bd8e27d697de328ba6d4fe7f676adaf4fc5223b1
Blurenis <jeremieondzaghe@gmail.com>
2025-10-23 11:39:17 +0200

    merge the multiply and divide feazture

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,21 @@ def subtract(*input):
         sum -= element
     
     return sum
+
+def multiply(*input):
+    product = 1
+    for element in input:
+        product *= element
+
+    return product
+
+
+def divide(*input):
+    if not input:
+        return None
+
+    quotient = input[0]
+    for element in input[1:]:
+        quotient /= element
+
+    return quotient
